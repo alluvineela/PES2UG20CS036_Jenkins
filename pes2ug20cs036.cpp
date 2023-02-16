87b276368ad8945cac2995a0cf08da9296171963
alluvineela <90202082+alluvineela@users.noreply.github.com>
2023-02-16 20:23:00 +0530

    Add files via upload

---
+++ pes2ug20cs036.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins----pes2ug20cs036\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
